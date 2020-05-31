e9f317fc100dcaf6d0e33740a825e64e22f06b74
734415 <42470457+LennisNgugi@users.noreply.github.com>
2020-05-31 06:25:37 +0100

    OLD method
    Feel free to ingnore the above code it was just a concept I was trying out

--- oldoutputs.py
+++ oldoutputs.py
@@ -1,12 +1,12 @@
 #Library 
-import os
+#import os
 # calling for output file which contains the caputered audio to text
-voice = open("output.txt",'w')
+#voice = open("output.txt",'w')
 # Reading the audio to text is the format voicecaptured ='MESSAGE' 
 #print("voicecapture = '{}'".format(voice.()))
 #voice.write('')
 # close 
 
-voice.write("voicecaputred = '")
+#voice.write("voicecaputred = '")
 
-voice.close()
+#voice.close()
